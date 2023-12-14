1001f538f84ecadc2b4d76456ab69999cb157cf0
Mahendra_Choudhary <mahendrajat072003@gmail.com>
2023-12-14 11:57:29 +0530

    third commit

--- routes/singlehotelRouter.js
+++ routes/singlehotelRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const singlehotelHandler = require("../controllers/singleHotelController");
+const singlehotelHandler = require("../controllers/singlehotelController");
 
 router.route("/:id").get(singlehotelHandler);
 
